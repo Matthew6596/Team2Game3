92c9b7d2464e8d41b03672ea3b872d67e2d95039
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2024-04-14 11:10:04 -0400

    Added stuff to GameManager

--- Team2Game3/Assets/Scripts/GameManager.cs
+++ Team2Game3/Assets/Scripts/GameManager.cs
@@ -20,7 +20,29 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    //Private Variables
+    [SerializeField]
+    private float timeToBeatGame;
+
     //Public Variables
     public List<ItemScript> playerItems = new();
+    public bool inCombat;
+    public int score;
+
+    //Things to remember when change scene
+    public float gameProgress;
+    public Vector2 playerPosition;
+
+    private void Update()
+    {
+        if (!inCombat)
+        {
+            gameProgress += Time.deltaTime;
+            if (gameProgress > timeToBeatGame)
+            {
+                //End game
+            }
+        }
+    }
 
 }
