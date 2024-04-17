3c7f7d87838565c42eb0a0064cba8682295194e6
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2024-04-16 21:59:34 -0400

    Good ending done
    Trianlge waifu

---
+++ Team2Game3/Assets/Scripts/EndingScript.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingScript : MonoBehaviour
+{
+    static public EndingScript inst;
+
+    public Transform player;
+    public Transform badShark;
+    public Transform fishyWifey;
+
+    public Transform[] playerLocations;
+    public Transform[] badLocations;
+    public Transform[] goodLocations;
+
+    public float playerSpd;
+    public float sharkSpd;
+    public float wifeySpd;
+
+    bool began = false;
+    bool goodEnding;
+
+    int sequence;
+
+    bool[] timedActionDone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inst = this;
+        timedActionDone = new bool[2];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (began)
+        {
+            if(goodEnding)
+                GoodEndSequence();
+            else
+                BadEndSequence();
+        }
+    }
+
+    public void BeginEnding(bool goodEnd)
+    {
+        sequence = 0;
+        goodEnding = goodEnd;
+        began = true;
+    }
+
+    //These update every frame!
+    void GoodEndSequence()
+    {
+        if (sequence >= 0)
+        {
+            player.position = Vector3.MoveTowards(player.position, playerLocations[0].position, playerSpd*Time.deltaTime);
+            StartTimedAction(() => { sequence++; }, 1, 0);
+        }
+        if (sequence >= 1)
+        {
+            fishyWifey.position = Vector3.MoveTowards(fishyWifey.position, goodLocations[0].position, wifeySpd*Time.deltaTime);
+            StartTimedAction(() => { 
+                //Instatiate heart 
+            }, 1, 1);
+        }
+    }
+    void BadEndSequence()
+    {
+
+    }
+
+    void StartTimedAction(Action act, float delay, int id)
+    {
+        if (!timedActionDone[id])
+        {
+            StartCoroutine(timedAction(act, delay));
+            timedActionDone[id] = true;
+        }
+    }
+
+    IEnumerator timedAction(Action act, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        act.Invoke();
+    }
+}
--- Team2Game3/Assets/Scripts/GameManager.cs
+++ Team2Game3/Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
@@ -24,10 +26,14 @@ public class GameManager : MonoBehaviour
     [Header("Options")]
     [SerializeField]
     private float timeToBeatGame;
+    [SerializeField]
+    private int goodEndingScoreMinimum;
     public int playerHp;
     public int playerMana;
     public int playerAttack;
     public MagicScript[] magicOptions;
+    public Transform progressBar;
+    public Transform canvas;
 
     //Public Variables
     [Space]
@@ -41,16 +47,45 @@ public class GameManager : MonoBehaviour
     public float gameProgress;
     public Vector2 playerPosition;
 
+    private void Start()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            progressBar.GetChild(i).GetChild(0).GetChild(0).gameObject.SetActive(false);
+        }
+    }
+
     private void Update()
     {
         if (!inCombat)
         {
             gameProgress += Time.deltaTime;
+            int numBarsToFill = (int)(gameProgress / timeToBeatGame * 20f);
+            for(int i=0; i<numBarsToFill; i++)
+            {
+                progressBar.GetChild(i).GetChild(0).GetChild(0).gameObject.SetActive(true);
+            }
             if (gameProgress > timeToBeatGame)
             {
-                //End game
+                EndGame();
             }
         }
     }
+    
+    void EndGame()
+    {
+        inCombat = true;
+        EndingScript.inst.BeginEnding(score >= goodEndingScoreMinimum);
+        StartCoroutine(HideSwimUI());
+    }
 
+    float uiSpd = 5f;
+    IEnumerator HideSwimUI()
+    {
+        while (canvas.localPosition.y < 100)
+        {
+            canvas.Translate(Time.deltaTime * uiSpd * Vector3.Distance(canvas.localPosition, Vector3.up * 100) * Vector3.up);
+            yield return null;
+        }
+    }
 }
